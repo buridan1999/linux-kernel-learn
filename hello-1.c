37749a8f1a14f1c62223dbd7aad63e4b1545965c
buridan <buridan1999@gmail.com>
2026-04-19 02:24:12 +0500

    hellow world kernel module

---
+++ hello-1.c
@@ -0,0 +1,25 @@
+/*
+ *  hello-1.c - The simplest kernel module.
+ */
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Hello world 1.\n");
+
+	/*
+	 * A non 0 return means init_module failed; module can't be loaded.
+	 */
+	return 0;
+}
+
+static void __exit hello_cleanup(void)
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
+
+MODULE_LICENSE("GPL");
